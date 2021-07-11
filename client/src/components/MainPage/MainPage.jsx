285c83cb0f68fb7d1fcd6b4d785a02f4dfa2c91e
Netta Avidan <nettavidan19@gmail.com>
2021-07-11 17:49:27 +0300

    styling

--- client/src/components/MainPage/MainPage.jsx
+++ client/src/components/MainPage/MainPage.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Login from '../Login/Login'
+import "./style/style.css"
+import run from "./style/jogging.jpg"
+import man from "./style/man.jpg"
 
 export default function MainPage() {
     return (
-        <div>
-            this is mainn <br />
-            hello welcome
-            {/* <Login></Login> */}
+        <div className="main">
+            {/* <div >
+                <button className="main-button">hello</button>
+            </div> */}
+            {/* <img src={man} alt="run" className="main-pic"/> */}
+            bla lsdfdsf
+
         </div>
     )
 }
