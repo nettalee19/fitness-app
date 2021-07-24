15ac47c03003735575b74dba22f937c7e19c015b
Netta Avidan <nettavidan19@gmail.com>
2021-07-24 17:20:36 +0300

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,11 +2,10 @@ const jwt = require('jsonwebtoken')
 const users= require('../models/users.models')
 
 const auth = async(req,res,next) =>{
-    //console.log('auth middleware2')
     // next()
     try{
         const token = req.header('Authorization').replace('Bearer ','')
-        // console.log(token)
+        
         const decoded = jwt.verify(token, 'user') //token valid and not expired
         //should be the same as the value in Models
         const user = await users.findOne({ _id:  decoded._id, 'tokens.token': token}) //find user with an id to match the auth token
